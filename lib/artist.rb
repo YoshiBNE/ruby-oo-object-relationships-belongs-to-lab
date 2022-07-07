9796840e3b8edd58816a0962d4d641009bee9ed4
YoshiBNE <yoshiyasu_express@me.com>
2022-07-08 00:14:56 +1000

    done

---
+++ lib/artist.rb
@@ -0,0 +1,9 @@
+class Artist
+
+    attr_accessor :name, :genre
+
+
+    def self.name(name)
+        @name = name
+    end
+end
---
+++ lib/post.rb
@@ -0,0 +1,9 @@
+class Post
+
+    attr_accessor :title, :author
+
+    def self.title(title)
+        @title = title
+    end
+
+end
